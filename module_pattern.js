d35ca9ec01ae4899ad15e8919a8740800bddbce5
ksaluja <ksaluja@haystax.com>
2017-05-03 12:08:02 -0400

    Global Functions

--- module_pattern.js
+++ module_pattern.js
@@ -2,3 +2,8 @@
 	// ... all vars and functions are in this scope only
 	// still maintains access to all globals
 }());
+
+
+(function ($, YAHOO) {
+	// now have access to globals jQuery (as $) and YAHOO in this code
+}(jQuery, YAHOO));
